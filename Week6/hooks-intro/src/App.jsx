264e8b9a35ba6c17396045288a4cdcfbb5f11531
Pulkit <pulkit161001@gmail.com>
2024-07-04 22:58:13 +0530

    useMemo, useCallback, useRef assignments in week 6

--- Week6/hooks-intro/src/App.jsx
+++ Week6/hooks-intro/src/App.jsx
@@ -1,11 +1,24 @@
-import { useState, memo, useEffect, useMemo, Children, useCallback } from "react";
+import { useState, memo, useEffect, useMemo, Children, useCallback, useRef } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
 
+  const taxContainer = useRef()
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      // document.getElementById("taxContainer").innerHTML = 10;
+      taxContainer.current.innerHTML = 10;
+    },5000)
+  },[])
+
+  const tax = 20000
+
   return (
     <div>
+      {/* hi there <div id="taxContainer">{tax}</div> */}
+      hi there <div ref={taxContainer}>{tax}</div>
     </div>
   );
 }
